e88267cf79767aa6aa7f5da6d291e9472bf449be
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2024-12-27 22:44:30 +0300

    cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -12,6 +13,10 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
   const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("Please select a size");
+      return;
+    }
     let cartData = structuredClone(cartItems);
     if (cartData[itemId])
       if (cartData[itemId][size]) {
@@ -26,6 +31,23 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData);
   };
 
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) { 
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+  return totalCount;
+}
+
+    
   const value = {
     products,
     currency,
@@ -36,11 +58,10 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
+    getCartCount,
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
   );
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 const Cart = () => {
-  return <div></div>;
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    console.log(tempData);
+  }, [cartItems]);
+
+  return (<div className="">
+
+
+  
+
+  </div>)
 };
 
 export default Cart;
