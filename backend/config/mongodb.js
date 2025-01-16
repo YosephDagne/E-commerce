7815e6b7eafcd266a6fbd7b1f123a7218e831b61
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-16 13:11:59 +0300

    connect to the mongo db

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,1 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+// const connectDB = () => {
+//   mongoose.connect(process.env.MONGODB_URI).then(() => {
+//     console.log("connected to db");
+//   });
+// };
+
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("connected to db");
+  });
+
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
 
 // App config
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+connectDB()
+
 
 // Middle wares
 app.use(express.json());
