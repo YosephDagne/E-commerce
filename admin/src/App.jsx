a6a39a33d46295fb57e945c8d49d0fea60c010d1
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-21 23:34:43 +0300

    nav bar for admin page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <NavBar/>
+    </>
   )
 }
 
---
+++ admin/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const NavBar = () => {
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img className="w-[max(5%,80px)]" src={assets.logo1} alt="" />
+      <button className="bg-gray-600 text-white px-2 py-5 sm:px-7 sm:py-2 rounded-full text-sm sm:text-sm mt-5">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
