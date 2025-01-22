e1337290f069757a052d5148a15563a6c991597e
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-22 11:18:53 +0300

    side bar for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-
+import React from "react";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders"
 const App = () => {
   return (
-    <>
-      <NavBar/>
-    </>
-  )
-}
+    <div className="bg-gray-50 min-h-screen ">
+      <NavBar />
+      <hr />
+      <div className="flex w-full">
+        <SideBar />
+        <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+          <Routes>
+            <Route path="/add" element={<Add />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/order" element={<Orders />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
