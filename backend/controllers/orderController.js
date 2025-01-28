9014c095c2d0f4a9573ccc494eb006ef6185af16
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-28 22:44:09 +0300

    ordercontroller and ordermodel

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,20 @@
+// Placing order cash on delivery method
+const placeOrder = async (req, res) => {};
+
+// Placing order using Stripe method
+const placeOrderStripe = async (req, res) => {};
+
+// Placing order using Razorpay method
+const placeOrderRazorpay = async (req, res) => {};
+
+// All Orders data for Admin Panel
+const allOrders = async (req, res) => {};
+
+// User Order data for Frontend
+const userOrders = async (req, res) => {};
+
+// Update Order Status for Admin Panel
+const updateStatus = async (req, res) => {};
+
+
+export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus};
