b1f082282d6a50cd3455e2fcf53e6f066bd6cb4f
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-29 21:21:57 +0300

    all user orders placeorder and order page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,7 +36,16 @@ const placeOrderRazorpay = async (req, res) => {
 
 // All Orders data for Admin Panel
 const allOrders = async (req, res) => {
-  // Implementation to fetch all orders for Admin
+
+  try {
+    const orders = await orderModel.find({})
+    res.json({ success: true, orders })
+    
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+    
+  }
 };
 
 // User Order data for Frontend
