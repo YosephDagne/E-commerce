a17916a80ddb6d1b11b26a8e19c00dc04d93d120
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-31 22:20:32 +0300

    status handler function

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -30,6 +30,22 @@ const Orders = ({ token }) => {
     }
   };
 
+  const statusHandler = async (event, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        { orderId, status: event.target.value },
+        { headers: { token } }
+      );
+      if (response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  };
+
   useEffect(() => {
     fetchAllOrders();
   }, [token]);
@@ -92,11 +108,15 @@ const Orders = ({ token }) => {
             </p>
 
             <div>
-              <select className="p-2 font-semibold">
+              <select
+                onChange={(event) => statusHandler(event, order._id)}
+                value={order.status}
+                className="p-2 font-semibold"
+              >
                 <option value="Order Placed">Order Placed</option>
                 <option value="Packing">Packing</option>
                 <option value="Shipped">Shipped</option>
-                <option value="Out for Delivery">Out for Delivery</option>
+                <option value="Out for delivery">Out for delivery</option>
                 <option value="Delivered">Delivered</option>
               </select>
             </div>
