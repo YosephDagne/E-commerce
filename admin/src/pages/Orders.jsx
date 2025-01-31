195808bf6bce6ab97f42a58bf93dfc1cbe93efb5
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-01-31 21:44:11 +0300

    layout

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -37,62 +37,69 @@ const Orders = ({ token }) => {
   return (
     <div>
       <h3>Order Page</h3>
+
       <div>
         {Orders.map((order, index) => (
-          <div className=" grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 md:my-4 text-sm sm:text-sm text-gray-700">
-            <div key={index}>
-              <img src={assets.parcel_icon} alt="" />
-
+          <div
+            key={index}
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr]
+                       gap-3 items-start border-4 border-gray-200 
+                       p-5 md:p-8 my-3 md:my-4 text-sm sm:text-sm text-gray-700"
+          >
+            <img className="w-12" src={assets.parcel_icon} alt="Parcel Icon" />
+            <div>
               <div>
                 {order.items.map((item, index) => {
                   if (index === order.items.length - 1) {
                     return (
-                      <p key={index}>
-                        {item.name} x {item.quantity} <span>{item.size}</span>
+                      <p className="py-0.5" key={index}>
+                        {item.name} x {item.quantity}
                       </p>
                     );
                   } else {
                     return (
-                      <p key={index}>
-                        {item.name} x {item.quantity} <span>{item.size}</span>,
+                      <p className="py-0.5" key={index}>
+                        {item.name} x {item.quantity},{" "}
                       </p>
                     );
                   }
                 })}
               </div>
-              <p>{order.address.firstName + "  " + order.address.lastName}</p>
               <div>
-                <p>{order.address.street + ","}</p>
-                <p>
-                  {order.address.city +
-                    ", " +
-                    order.address.state +
-                    "  " +
-                    order.address.country +
-                    ", " +
-                    order.address.zipCode}
+                <p className="mt-3 mb-2 font-medium">
+                  {order.address.firstName} {order.address.lastName}
                 </p>
+                <p>{order.address.street},</p>
+                <p>
+                  {order.address.city}, {order.address.state},{" "}
+                  {order.address.country}, {order.address.zipCode}
+                </p>
+                <p>{order.address.phone}</p>
               </div>
-              <p>{order.address.phone}</p>
             </div>
 
             <div>
-              <p>Items: {order.items.length}</p>
-              <p>Method: {order.paymentMethod}</p>
+              <p className="text-sm sm:text-[15px]">
+                Items: {order.items.length}
+              </p>
+              <p className="mt-3">Method: {order.paymentMethod}</p>
               <p>Payment: {order.payment ? "Done" : "Pending"}</p>
               <p>Date: {new Date(order.date).toLocaleDateString()}</p>
             </div>
-            <p>
+            <p className="text-sm sm:text-[15px]">
               {currency}
               {order.amount}
             </p>
-            <select>
-              <option value="Order Placed">Order Placed</option>
-              <option value="Packing">Packing</option>
-              <option value="Shipped">Shipped</option>
-              <option value="Out for Delivery">Out for Delivery</option>
-              <option value="Delivered">Delivered</option>
-            </select>
+
+            <div>
+              <select className="p-2 font-semibold">
+                <option value="Order Placed">Order Placed</option>
+                <option value="Packing">Packing</option>
+                <option value="Shipped">Shipped</option>
+                <option value="Out for Delivery">Out for Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
           </div>
         ))}
       </div>
