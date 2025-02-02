6946052280ff025b3be019f865e92ba67e9ebcdf
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-02 11:08:47 +0300

    verify stripe

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const verify = () => {
+const Verify = () => {
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
+const [searchParams, setSearchParams] = useSearchParams();
+
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+
+  const verifyPayment = async () => {
+    try {
+      if (!token) {
+        return null;
+      }
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyStripeOrder",
+        { success, orderId },
+        { headers: { token } }
+      );
+
+
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/cart");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, [token]);
   return (
     <div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default verify
+export default Verify;
