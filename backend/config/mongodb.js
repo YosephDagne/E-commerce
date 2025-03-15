87deaa7ed30e0e66fc21e37818ee265c402339a7
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-15 22:30:27 +0300

    evv

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("connected to database");
-  });
+  try {
+    // Use your MongoDB URI to connect to the database
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+    // Log success
+    console.log("Connected to the database");
+  } catch (error) {
+    // Log any errors
+    console.error("Error connecting to database:", error);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 export default connectDB;
