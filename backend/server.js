1da79e0c5072025ce5163d1f1d6dac15de8ee35b
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-16 17:30:06 +0300

    fix the db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config"; // Loads environment variables from .env
+import "dotenv/config"; 
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
