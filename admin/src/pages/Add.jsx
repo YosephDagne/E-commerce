b72c027d3f96353130381f238962d91b28ac0924
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-24 20:38:48 +0300

    add button

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -286,7 +286,7 @@ const Add = ({ token }) => {
           Add to bestseller
         </label>
       </div>
-      <button className="bg-gray-700 text-white px-4 py-2 text-md font-semibold rounded-full ring-2 ring-offset-gray-700 shadow-md border-2 border-black hover:bg-green-500 hover:scale-105 transition-all duration-300 ease-in-out active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500">
+      <button className="bg-gray-700 text-white px-6 py-2 text-md font-semibold rounded-full ring-2 ring-offset-gray-700 shadow-md border-2 border-black hover:bg-green-500 hover:scale-105 transition-all duration-300 ease-in-out active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500">
         ADD
       </button>
     </form>
