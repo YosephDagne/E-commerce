fec6bb7bce4de199ccd334861d4405faccfccb0d
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-24 16:44:55 +0300

    styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
           href="https://e-commerce-admin-roan-rho.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-green-700 text-white px-6 py-2.5 text-md font-semibold rounded-full ring-2 ring-green-600 shadow-md border-2 border-black hover:bg-green-500 hover:scale-105 transition-all duration-300 ease-in-out active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+          className="bg-gray-700 text-white px-4 py-2 text-md font-semibold rounded-full ring-2 ring-offset-gray-700 shadow-md border-2 border-black hover:bg-green-500 hover:scale-105 transition-all duration-300 ease-in-out active:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
         >
           Admin Panel
         </a>
