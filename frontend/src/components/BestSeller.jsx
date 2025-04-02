8e57ffa57359a2020e4afbeb533d4574e709db03
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-04-02 08:49:18 +0300

    syling best seller

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -14,18 +14,18 @@ const BestSeller = () => {
   }, [products]);
 
   return (
-    <div className="my-10">
+    <div className="my-10 container mx-auto px-4">
       <div className="text-center text-3xl py-8">
-        <Title Text1={"BEST"} Text2={"SELLER"} />
-        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 ">
+        <Title Text1="BEST" Text2="SELLER" />
+        <p className="max-w-2xl mx-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          soluta maxime pariatur. !
+          soluta maxime pariatur!
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {bestseller.map((item, index) => (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-8">
+        {bestseller.map((item) => (
           <ProductItem
-            key={index}
+            key={item._id}
             id={item._id}
             name={item.name}
             image={item.image}
